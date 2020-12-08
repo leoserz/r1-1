a65615ed8af5bb551a82536a9c22308cb7789a4c
Konstantin Sheremetev <75672804+progtips@users.noreply.github.com>
2020-12-08 17:02:23 +0300

    Первая версия кода
    Добавлены три файла

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php require_once "main.php"; ?>
+<!--
+	4.2 Эффективные методы программирования
+-->
+	<head>
+		<title>Пример b42</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css" >
+	</head>
+	<body>
+
+		<div id="wrapper">
+			<div id="header">
+				<h2>Работа с Git</h2>
+			</div> 
+
+			<div id="content">
+
+				<h2>Список файлов в проекте</h2>
+				<?php ShowFiles(); ?>
+
+			</div>
+			<div id="footer">
+			</div>
+		</div>	
+		
+	</body>
+</html>
+
